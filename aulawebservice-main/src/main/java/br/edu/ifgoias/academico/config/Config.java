b998a777efea5755a2e2e46abed7738743ec5fd5
vitor-csilva <56806562+vitor-csilva@users.noreply.github.com>
2020-10-31 23:21:32 -0300

    Add files via upload

---
+++ aulawebservice-main/src/main/java/br/edu/ifgoias/academico/config/Config.java
@@ -0,0 +1,27 @@
+package br.edu.ifgoias.academico.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import br.edu.ifgoias.academico.entities.Aluno;
+import br.edu.ifgoias.academico.repositories.AlunoRepository;
+
+@Configuration
+public class Config implements CommandLineRunner {
+    
+	@Autowired
+	private AlunoRepository alunoRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		Aluno a1 = new Aluno(null, "Vitor", null, null);
+		Aluno a2 = new Aluno(null, "Dory", null, null);
+		
+		alunoRepository.save(a1);
+		alunoRepository.save(a2);
+	}
+
+}
---
+++ aulawebservice-main/src/main/java/br/edu/ifgoias/academico/services/CursoService.java
@@ -0,0 +1,48 @@
+package br.edu.ifgoias.academico.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.edu.ifgoias.academico.entities.Curso;
+import br.edu.ifgoias.academico.repositories.CursoRepository;
+
+@Service
+public class CursoService {
+
+		@Autowired
+		private CursoRepository repository;
+		
+		public List<Curso> findAll(){
+				return repository.findAll();
+		}
+		
+		public Curso findById(Integer id) {
+			return repository.findById(id).orElseThrow(
+						() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		}
+		
+		public Curso insert(Curso obj) {
+			
+			return repository.save(obj);
+		}
+		
+		public void delete (Integer id, Curso obj) {
+			repository.findById(id).map(
+					curso -> {repository.delete(curso);
+					           return Void.TYPE;
+					         }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		}
+		
+		public void update(Integer id, Curso obj) {
+			repository.findById(id).map(
+					curso -> {curso.setNomecurso(obj.getNomecurso());
+					           return repository.save(curso);
+					         }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+			
+		}
+		
+}
